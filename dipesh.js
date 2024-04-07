a799af8ab361bdd91e1d8670d3d5e03b9beab965
dipesh subedi <dipeshfitness74@gmail.com>
2024-04-08 01:33:24 +0545

    first commit

---
+++ dipesh.js
@@ -0,0 +1,18 @@
+//javascript course
+//convert yo
+
+console.log("------Money converter online==------");
+console.log("------Convert your dollars in rupees==------");
+console.log("------created- by Dipesh Subedi-----"); 
+let dollar;
+prompt("Enter your dollar amount:",dollar);
+let nepAmount = dollar * 80;
+console.log("Your nepali rupeee is :",nepAmount);
+
+
+console.log("------Convert your  rupees into dollars==------");
+let nepsAmount;
+prompt("Enter your nepali amount:",nepsAmount);
+ 
+let dollars = nepsAmount / 80;
+console.log("your dollar is :",dollars)
